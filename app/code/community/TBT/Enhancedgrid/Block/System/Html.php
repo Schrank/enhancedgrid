f568787868bd50fbf52cfd4f73fa19b4b3b4f396
Chris <christian@milkycode.com>
2017-05-04 01:03:07 +0200

    Removed notification in config.

--- app/code/community/TBT/Enhancedgrid/Block/System/Html.php
+++ app/code/community/TBT/Enhancedgrid/Block/System/Html.php
@@ -38,7 +38,7 @@ class TBT_Enhancedgrid_Block_System_Html
         	<div style=" margin-bottom: 12px; width: 430px;">
             Enhanced Grid v'.Mage::getConfig()->getNode('modules/TBT_Enhancedgrid/version').". <a href='https://github.com/jayelkaake/enhancedgrid' target='_blank'>Click here for updates.</a><BR />
 		";
-        $html .= Mage::getBlockSingleton('enhancedgrid/widget_loyalty')->toHtml();
+        //$html .= Mage::getBlockSingleton('enhancedgrid/widget_loyalty')->toHtml();
         $html .= '
             </div>
         ';
